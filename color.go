7cf1d5a0a4aac6c44a6163b0fc09bc2e0d7c41d4
Your Name Here <you@example.com>
2023-10-06 06:33:02 -0700

    changes

--- color.go
+++
@@ -1,5 +0,0 @@
-package ansi
-
-type Color int
-
-const ()
--- coordinate.go
+++ coordinate.go
@@ -3,5 +3,8 @@ package ansi
 type Coordinate struct {
 	X int
 	Y int
-	Z int
+	// TODO
+	// Z only makes sense if we are talking about multiple layers, but in those
+	// cases we just usually keep a shadow buffer
+	//Z int
 }
